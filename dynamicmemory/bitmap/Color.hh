0d9bfa17e098e99f4a5a63e0fa6a3b491e82c4ef
shaopengge <sge2@stevens.edu>
2018-11-09 10:46:14 -0500

    s

--- dynamicmemory/bitmap/Color.hh
+++ dynamicmemory/bitmap/Color.hh
@@ -1,10 +1,27 @@
 #pragma once
 
-class Color {
+class Color{
 private:
-  uint32_t rgba;
+	uint32_t rgba;
 public:
-	Color(uint8_t r, uint8_t g, uint8_t b) {}
-	//TODO:	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha) {}
-	
+	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 1) {
+		rgba = ((r & 0xff) << 24) + ((g & 0xff) << 16) + ((b & 0xff) << 8) + (alpha & 0xff);
+	}
+
+	char getColor()  {
+		if (rgba == 0xff000001)
+			return 'R';
+		else if (rgba == 0xff0001)
+			return 'B';
+		else if (rgba == 0xff01)
+			return 'G';
+		else if (rgba == 0xffffff01)
+			return 'W';
+		else if (rgba == 0xffff0001)
+			return 'Y';
+		else if (rgba == 0x1)
+			return 'K';
+		else
+			return  'X';
+	}
 };
--- dynamicmemory/bitmap/main.cc
+++ dynamicmemory/bitmap/main.cc
@@ -2,41 +2,109 @@
 #include "Color.hh"
 using namespace std;
 
+class Bitmap{
+private:
+	uint32_t width,height;
+	char *p;
+public:
+	Bitmap(uint32_t w, uint32_t h) : width(w), height(h), p(new char[width * height]) {
+		for (int i = 0; i < width * height; i++)
+			p[i] = ' ';
+	}
+
+	~Bitmap(){
+		delete [] p;
+	}
+	void plot(int x, int y, Color c){
+		p[y*width + x] = c.getColor();
+	}
+
+	void line(const int& x0, const int& y0, const int& x1, const int& y1, const Color& c){
+		int dx = x1 - x0;
+		int dy = y1 - y0;
+		double error = 0.0;
+		double deltaerr = double(dy) / double(dx);
+		int y = y0;
+		for (int x = x0; x <= x1; ++x){
+			plot(x,y,c);
+			error += deltaerr;
+			if (abs(error) >= 0.5){
+				y += 1;
+				error -= 1.0;
+			}
+		}
+	}
+
+	void horizLine(const int& x0, const int& x1, const int& y, const Color& c){
+		for(int x = x0; x <= x1; ++x)
+			plot(x,y,c);
+	}
+
+	void vertLine(const int& x, const int& y0, const int& y1, const Color& c){
+		for(int y = y0; y <= y1; ++y)
+			plot(x,y,c);
+	}
+
+	void drawRect(const int& x, const int& y, const uint& w, const uint& h, const Color& c){
+		horizLine(x, x + w, y, c);
+		vertLine(x, y, y + h, c);
+		horizLine(x, x + w, y + h, c);
+		vertLine(x + w, y, y + h, c);
+	}
+
+	void fillRect(const int& x0, const int& y0, const uint& w, const uint& h, const Color& c){
+		for(int x = x0; x <= x0 + w; ++x){
+			for(int y = y0; y <= y0 + h; ++y){
+				plot(x,y,c);
+			}
+		}
+	}
+
+	void ellipse(const int& x0, const int& y0, const uint& w, const uint& h, const Color& c) {
+		int y1,y2;
+		for (int x = x0 - w / 2; x <= x0 + w / 2; ++x) {
+			y1 = round(sqrt((1-4.0*(x-x0)*(x-x0)/(w*w))*(h*h/4.0))+y0);
+			y2 = y0 - (y1-y0);
+			plot(x,y1,c);
+			plot(x,y2,c);
+		}
+	}
+
+
+
+
+	friend ostream& operator <<(ostream& s, const Bitmap& b){
+		for(int i=0; i<b.height; i++){
+			for(int j=0; j<b.width; j++){
+				s <<  b.p[i * b.width + j] << "  ";
+			}
+			cout<<'\n';
+		}
+		return s;
+	}
+
+};
+
+
+
+
+
+
+
 int main() {
-	Bitmap b(30,20); // 30 pixels across, 20 pixels high, each pixel RGBA
-	// top left pixel = (0,0)
+	Bitmap b(30,20); // 30 pixels across, 20 pixels high, each pixel RGBA   // top left pixel = (0,0)
 	Color RED(255,0,0); // all red, no green, no blue (fully opaque)
 	Color BLUE(0,255,0);
 	Color GREEN(0,0,255);
 	Color WHITE(255,255,255);
-	
-	
-	b.line(0,0,   19,19, RED);
-	b.line(0,5,   29,10, BLUE); //Bresenham algorithm
-	//https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+	Color BLACK(0,0,0);
 
-	// https://en.wikipedia.org/wiki/Xiaolin_Wu%27s_line_algorithm
-	//TODO: b.line(0,100, 100,50, BLUE); //Wu algorithm
+	b.line(0,0,19,19, RED);
+	b.line(0,5,29,10, BLUE);
 	b.horizLine(0, 20, 19, GREEN); // from x=0 to x=20 at y=19
 	b.vertLine(5, 0,19, GREEN); // from y = 0 to y=19 at x = 5
 	b.fillRect(10,10, 4, 3,BLACK); // x = 10, y =10 w=4, h=3
 	b.drawRect(10,10, 4, 3,WHITE); // x = 10, y =10 w=4, h=3
-  b.ellipse(15,0, 8, 5, RED);    // ellipse centered at (15,0) w= 8, h=5
+	b.ellipse(23,5, 8, 5, RED);    // ellipse centered at (23,5) w= 8, h=5
 	cout << b;
-	/*
-R
- R
-  R
-   R
-    R
-BBB
-   BBBBB
-        BBBBBB
-
-
-	 */
 }
-
-
-
-	
